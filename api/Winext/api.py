b3d8096a73f343a126982e1ab511158144971403
Alejo <alejandro3688@hotmail.com>
2024-04-10 22:58:29 -0400

    Cambios

--- api/Winext/api.py
+++ api/Winext/api.py
@@ -1,10 +1,11 @@
 from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
+from django_softdelete.models import SoftDeletionModel
 from rest_framework.views import APIView
 from .serializers import *
 from .models import User, Role
 
-class UserIndexAPIView(APIView):
+class UserIndexAPIView(APIView, SoftDeletionModel):
     # Muestra todos los datos del model user, y lanza error si no lo encuentra
     def get(self, request):
         try:
@@ -13,8 +14,6 @@ class UserIndexAPIView(APIView):
                 return Response({"error": "No se encontraron usuarios"}, status=404)
             serializer = UserSerializer(users, many=True)
             return Response({"users": serializer.data})
-        except ObjectDoesNotExist:
-            return Response({"error": "No se encontró el usuario"}, status=404)
         except Exception as e:
             return Response({"error": str(e)}, status=500)
     
